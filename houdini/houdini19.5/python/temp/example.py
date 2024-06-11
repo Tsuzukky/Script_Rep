44f59a4afc6cb71b8e3d6a0ea4d2980ad5fb020a
Masato Tsuzuki <msjbinc@gmail.com>
2024-06-11 12:09:43 -0700

    Update example.py

--- houdini/houdini19.5/python/temp/example.py
+++ houdini/houdini19.5/python/temp/example.py
@@ -9,3 +9,17 @@ g = node.parmTemplateGroup()
 p = hou.FloatParmTemplate("name","label",3,[1,1,1])   #_init_("name",'label',.......)
 g.append(p)
 node.setParmTemplateGroup(g)
+
+
+#create Flipbooks
+cur_desktop = hou.ui.curDesktop()
+scene_viewer = hou.paneTabType.SceneViewer
+scene = cur_desktop.paneTabOfType(scene_viewer)
+scene.flipbookSettings().stash()
+flip_book_options = scene.flipbookSettings()
+
+flip_book_options.output('A:\\flipbook.$F4.jpeg') # Provide flipbook full path with padding.
+flip_book_options.frameRange((0, 5)) # Enter Frame Range Here in x & y
+flip_book_options.useResolution(1)
+flip_book_options.resolution((1080, 720)) # Based on your camera resolution
+scene.flipbook(scene.curViewport(), flip_book_options)

e800154a7b54af6f463e774011fc4b9b76ab7119
Masato Tsuzuki <msjbinc@gmail.com>
2024-06-11 11:59:38 -0700

    Update example.py

--- houdini/houdini19.5/python/temp/example.py
+++ houdini/houdini19.5/python/temp/example.py
@@ -1,1 +1,11 @@
+#bind to parent node
+ hou.parent()
+#bind yo node name from pythonSOP
+ hou.pwd()
 
+#create parameters for python
+node = hou.pwd()
+g = node.parmTemplateGroup()
+p = hou.FloatParmTemplate("name","label",3,[1,1,1])   #_init_("name",'label',.......)
+g.append(p)
+node.setParmTemplateGroup(g)

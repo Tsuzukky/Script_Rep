818bafa726c245b4c2397ed70bc4c5bcc8702bfd
Masato Tsuzuki <msjbinc@gmail.com>
2024-06-11 14:22:26 -0700

    Update DisneyFxSceneMgr.py

--- houdini/houdini19.5/python/temp/DisneyFxSceneMgr.py
+++ houdini/houdini19.5/python/temp/DisneyFxSceneMgr.py
@@ -5,4 +5,7 @@ import hou
 def make_stage():
   #GetToCurrentContextfromSOP
   getParentContext = hou.pwd().parent() 
+  #createNode("node_name,new_name)
+  lopstageNode = getParentContext.createNode('lopnet','stage')
+  
   

476efc238458146b292684997ba025f6757780c2
Masato Tsuzuki <msjbinc@gmail.com>
2024-06-12 17:21:01 -0700

    Update DisneyFxSceneMgr.py

--- houdini/houdini19.5/python/temp/DisneyFxSceneMgr.py
+++ houdini/houdini19.5/python/temp/DisneyFxSceneMgr.py
@@ -24,7 +24,76 @@ def make_stage():
         stageNode.setColor(hou.Color((0.5,0.4,0.0)))
             
                     
-               
+# version h.19.5.919
+import hou
+
+def make_stage():
+    current_context = hou.pwd().parent()
+    
+    #serch_currentContextNodes
+    nodeResource = current_context.glob("*")
+    nodeNamelist =[]
+    
+    for i in  nodeResource:
+    
+       nodeNamelist.append(i.name())
+        
+        
+    if 'stage' in nodeNamelist :
+                 
+        pass
+        
+    else:
+        stageNode = current_context.createNode('lopnet','stage')
+        stageNode.setColor(hou.Color((0.5,0.4,0.0)))
+            
+        
+def make_ROP():
+    current_context = hou.pwd().parent()
+    
+    #serch_currentContextNodes
+    nodeResource = current_context.glob("*")
+    nodeNamelist =[]
+    
+    for i in  nodeResource:
+    
+       nodeNamelist.append(i.name())
+        
+        
+    if 'ROP' in nodeNamelist :
+                 
+        pass
+        
+    else:
+        stageNode = current_context.createNode('ropnet','ROP')
+        stageNode.setColor(hou.Color((0.5,0.4,0.0)))
+           
+            
+           
+        
+           
+        
+        
+   
+        
+        
+        
+                  
+  
+make_stage()
+
+make_ROP()
+
+
+  
+
+
+    
+
+
+
+
+
   
 make_stage()
 

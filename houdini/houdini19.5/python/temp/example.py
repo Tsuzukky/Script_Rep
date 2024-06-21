08b4c39a993ce873ce52ccc595a43d85013ba4c6
Masato Tsuzuki <msjbinc@gmail.com>
2024-06-21 14:03:22 -0700

    Update example.py

--- houdini/houdini19.5/python/temp/example.py
+++ houdini/houdini19.5/python/temp/example.py
@@ -1,61 +1,83 @@
-#bind to parent node
- hou.parent()
-#bind yo node name from pythonSOP
- hou.pwd()
-
-#create parameters for python
-node = hou.pwd()
-g = node.parmTemplateGroup()
-p = hou.FloatParmTemplate("name","label",3,[1,1,1])   #_init_("name",'label',.......)
-g.append(p)
-node.setParmTemplateGroup(g)
-
-
-#create Flipbooks
-cur_desktop = hou.ui.curDesktop()
-scene_viewer = hou.paneTabType.SceneViewer
-scene = cur_desktop.paneTabOfType(scene_viewer)
-scene.flipbookSettings().stash()
-flip_book_options = scene.flipbookSettings()
-
-flip_book_options.output('A:\\flipbook.$F4.jpeg') # Provide flipbook full path with padding.
-flip_book_options.frameRange((0, 5)) # Enter Frame Range Here in x & y
-flip_book_options.useResolution(1)
-flip_book_options.resolution((1080, 720)) # Based on your camera resolution
-scene.flipbook(scene.curViewport(), flip_book_options)
-
-
-#Interapet Progress 
-hou.InterruptableOperation class
-Use this class to turn any Python code block into an interruptable operation.
-
-Note
-
-hou.InterruptableOperation does not work in the Python Shell. For testing purposes, use a shelf tool instead.
-
-METHODS
-Filter
-
-Original order
- Collapse AllExpand All
-__init__(operation_name, long_operation_name=None, open_interrupt_dialog=False)
-
-Construct a new InterruptableOperation.
-updateLongProgress(percentage=-1.0, long_op_status=None)
-
-Update the progress percentage and status of the long, or high-level, operation.
-updateProgress(percentage=-1.0)
-
-Update the progress percentage of the operation.
-
-
-
-##Selection Nodes##
-#Import Houdini's Command Module
+# version h.19.5.919
 import hou
-#Store path to Subnet in variable "rig"
-rig = hou.node("/obj/geo1")
-#For Loop - iterate through network, in search of nodes that match pattern "*_anim"
-for ctrl in rig.glob("*foo*"):
-    #Select all nodes that match pattern "*_anim" | NOTE: ctrl.setSelected(1, 0) also works :)
-    ctrl.setSelected(True, clear_all_selected=False)
+
+
+
+
+def make_stage():
+    current_context = hou.pwd().parent()
+    currentNodepos = hou.pwd().position()
+    
+    #serch_currentContextNodes
+    nodeResource = current_context.glob("*")
+    nodeNamelist =[]
+    
+    for i in  nodeResource:
+    
+       nodeNamelist.append(i.name())
+        
+        
+    if 'stage' in nodeNamelist :
+                 
+        pass
+        
+    else:
+        stageNode = current_context.createNode('lopnet','stage')
+        stageNode.setColor(hou.Color((0.5,0.4,0.0)))
+        #Organize nodeposition
+        stageNode.shiftPosition(currentNodepos)
+        stageNode.move([-1.5,-1])
+        
+        #stageNode.
+        #inside_Stage_inside
+        stageContext = hou.node(stageNode.path())
+        
+
+        makeLooadShot = stageContext.createNode('dpipe_LoadShot_v1')
+        makeconfigurenode =  makeLooadShot.createOutputNode('configurestage','loadmask_asset')
+        makenullnode =  makeconfigurenode .createOutputNode('null','SHOT')
+        makenullnode.setDisplayFlag(1)
+        
+        #update parms #you can check parmname from parmName.eval()
+        makeconfigurenode.setParms({'editload':'addremove'})
+        
+def make_ROP():
+    
+    current_context = hou.pwd().parent()
+    currentNodepos = hou.pwd().position()
+    
+    #serch_currentContextNodes
+    nodeResource = current_context.glob("*")
+    nodeNamelist =[]
+    
+    for i in  nodeResource:
+    
+       nodeNamelist.append(i.name())
+        
+        
+    if 'ROP' in nodeNamelist :
+                 
+        pass
+        
+    else:
+        ROPNode = current_context.createNode('ropnet','ROP')
+        #Organize nodeposition
+        ROPNode.shiftPosition(currentNodepos)
+        ROPNode.move([1.5,-1])
+        
+     
+     
+def make_parameters():
+    
+    #create parameters for python
+    node = hou.pwd()
+    parmtempGroup = node.parmTemplateGroup()
+    #ButtomParmTemplate
+    parm1 = hou.ButtonParmTemplate('Build_structure','Build_structure')
+    parm2 = hou.LabelParmTemplate(
+    parmtempGroup.append(parm1)
+    node.setParmTemplateGroup(parmtempGroup)
+
+    
+    
+make_parameters()
